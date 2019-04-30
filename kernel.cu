f4a9553f666a83a0ff3274742a45292314fa051c
sondrehav <sondre.havellen@gmail.com>
2019-04-30 15:44:51 +0200

    Optimizations

--- kernel.cu
+++ kernel.cu
@@ -17,15 +17,16 @@
 #define STEP_SIZE 3.83e-4
 
 void ppm(float* data, int width, int height, const std::string& path);
-void writeOutput(float4* data, int width, int height, const std::string& path);
+void writeOutput(float* data, int width, int height, const std::string& path);
 
 // static __device__ __forceinline__ void surf2Dwrite(T val, surface<void, cudaSurfaceType2D> surf, int x, int y, int s, enum cudaSurfaceBoundaryMode mode = cudaBoundaryModeTrap)
 // 2D surfaces
-surface<void, 2> inputSurfRef;
-surface<void, 2> outputSurfRef;
+surface<void, 2> velocitySurface;
+surface<void, 2> pressureSurface;
 
 // --- 2D float4 texture
-texture<float4, cudaTextureType2D, cudaReadModeElementType> texRef;
+texture<float2, cudaTextureType2D, cudaReadModeElementType> velocityTexRef;
+texture<float, cudaTextureType2D, cudaReadModeElementType> pressureTexRef;
 
 /*
  * Function for transforming to normalized sampler locations.
@@ -42,12 +43,12 @@ float __device__ divergence(float x, float y, int width, int height)
 	float2 xSampler = sl(x + 1.0, y, width, height);
 	float2 ySampler = sl(x, y + 1.0, width, height);
 	
-	float4 valuesOrig = tex2D(texRef, origSampler.x, origSampler.y);
-	float4 valuesX = tex2D(texRef, xSampler.x, xSampler.y);
-	float4 valuesY = tex2D(texRef, ySampler.x, ySampler.y);
+	float2 valuesOrig = tex2D(velocityTexRef, origSampler.x, origSampler.y);
+	float2 valuesX = tex2D(velocityTexRef, xSampler.x, xSampler.y);
+	float2 valuesY = tex2D(velocityTexRef, ySampler.x, ySampler.y);
 	
-	float vx = valuesX.y - valuesOrig.y;
-	float vy = valuesY.z - valuesOrig.z;
+	float vx = valuesX.x - valuesOrig.x;
+	float vy = valuesY.y - valuesOrig.y;
 	
 	return vx + vy;
 }
@@ -58,12 +59,12 @@ float2 __device__ gradient(float x, float y, int width, int height)
 	float2 xSampler = sl(x - 1.0, y, width, height);
 	float2 ySampler = sl(x, y - 1.0, width, height);
 
-	float4 valuesOrig = tex2D(texRef, origSampler.x, origSampler.y);
-	float4 valuesX = tex2D(texRef, xSampler.x, xSampler.y);
-	float4 valuesY = tex2D(texRef, ySampler.x, ySampler.y);
+	float valuesOrig = tex2D(pressureTexRef, origSampler.x, origSampler.y);
+	float valuesX = tex2D(pressureTexRef, xSampler.x, xSampler.y);
+	float valuesY = tex2D(pressureTexRef, ySampler.x, ySampler.y);
 
-	float vx = valuesOrig.x - valuesX.x;
-	float vy = valuesOrig.x - valuesY.x;
+	float vx = valuesOrig - valuesX;
+	float vy = valuesOrig - valuesY;
 	
 	return make_float2(vx, vy);
 }
@@ -80,15 +81,15 @@ __global__ void iteratePressure(int width, int height)
 	{
 		float2 sampler = sl(x, y, width, height);
 
-		float4 values = tex2D(texRef, sampler.x, sampler.y);
+		float values = tex2D(pressureTexRef, sampler.x, sampler.y);
 
 		float div = divergence(x, y, width, height);
 		float pressure = TIME_STEP * PRESSURE * SOUND_VELOCITY * SOUND_VELOCITY * div / STEP_SIZE;
 
 			// pressure valid
-		values.x -= pressure;
+		values -= pressure;
 
-		surf2Dwrite<float4>(values, outputSurfRef, x * sizeof(float4), y);
+		surf2Dwrite<float>(values, pressureSurface, x * sizeof(float), y);
 
 	}
 }
@@ -103,7 +104,7 @@ __global__ void iterateVelocity(int width, int height)
 	{
 
 		float2 sampler = sl(x, y, width, height);
-		float4 values = tex2D(texRef, sampler.x, sampler.y);
+		float2 values = tex2D(velocityTexRef, sampler.x, sampler.y);
 		float2 grad = gradient(x, y, width, height);
 		
 		float2 velocity = TIME_STEP * grad / (PRESSURE);
@@ -111,15 +112,15 @@ __global__ void iterateVelocity(int width, int height)
 		if (y > 0)
 		{
 			// x valid
-			values.y -= velocity.x;
+			values.x -= velocity.x;
 		}
 
 		if (x > 0)
 		{
 			// y valid
-			values.z -= velocity.y;
+			values.y -= velocity.y;
 		}
-		surf2Dwrite<float4>(values, outputSurfRef, x * sizeof(float4), y);
+		surf2Dwrite<float2>(values, velocitySurface, x * sizeof(float2), y);
 
 	}
 }
@@ -132,8 +133,9 @@ __global__ void firstVelocityIteration(int width, int height)
 	unsigned int y = blockIdx.y * blockDim.y + threadIdx.y;
 	if (x < width && y < height)
 	{
+
 		float2 sampler = sl(x, y, width, height);
-		float4 values = tex2D(texRef, sampler.x, sampler.y);
+		float2 values = tex2D(velocityTexRef, sampler.x, sampler.y);
 		float2 grad = gradient(x, y, width, height);
 
 		float2 velocity = 0.5 * TIME_STEP * grad / (PRESSURE);
@@ -141,43 +143,16 @@ __global__ void firstVelocityIteration(int width, int height)
 		if (y > 0)
 		{
 			// x valid
-			values.y -= velocity.x;
+			values.x -= velocity.x;
 		}
 
 		if (x > 0)
 		{
 			// y valid
-			values.z -= velocity.y;
+			values.y -= velocity.y;
 		}
-		surf2Dwrite<float4>(values, outputSurfRef, x * sizeof(float4), y);
+		surf2Dwrite<float2>(values, velocitySurface, x * sizeof(float2), y);
 
-	}
-}
-
-void __global__ transformTest(int width, int height)
-{
-	unsigned int x = blockIdx.x * blockDim.x + threadIdx.x;
-	unsigned int y = blockIdx.y * blockDim.y + threadIdx.y;
-	if (x < width && y < height)
-	{
-		float2 sampler1 = sl(x + 1, y + 1, width, height);
-		float2 sampler2 = sl(x + 1, y - 1, width, height);
-		float2 sampler3 = sl(x - 1, y - 1, width, height);
-		float2 sampler4 = sl(x - 1, y + 1, width, height);
-		float4 values = tex2D(texRef, sampler1.x, sampler1.y) + tex2D(texRef, sampler2.x, sampler2.y) + tex2D(texRef, sampler3.x, sampler3.y) + tex2D(texRef, sampler4.x, sampler4.y);
-		surf2Dwrite<float4>(values / 4, outputSurfRef, x * sizeof(float4), y);
-	}
-}
-
-__global__ void writeBack(int width, int height)
-{
-	// Calculate surface coordinates
-	unsigned int x = blockIdx.x * blockDim.x + threadIdx.x;
-	unsigned int y = blockIdx.y * blockDim.y + threadIdx.y;
-	if (x < width && y < height)
-	{
-		float4 value = surf2Dread<float4>(outputSurfRef, x * sizeof(float4), y);
-		surf2Dwrite<float4>(value, inputSurfRef, x * sizeof(float4), y);
 	}
 }
 
@@ -194,8 +169,11 @@ int main()
 {
 
 	const size_t width = 256, height = 128;
-	float4* h_data = new float4[width * height];
-	size_t size = width * height * sizeof(float4);
+	float* h_pressureData = new float[width * height];
+	float2* h_velocityData = new float2[width * height];
+
+	size_t pressureArraySize = width * height * sizeof(float);
+	size_t velocityArraySize = width * height * sizeof(float2);
 	/*
 	float4 min = make_float4(std::numeric_limits<float>::max());
 	float4 max = make_float4(std::numeric_limits<float>::min());
@@ -218,36 +196,51 @@ int main()
 	{
 		h_data[i * width + j].x = ((h_data[i * width + j] - min) / (max - min) - 0.5).x;
 	}*/
-	std::fill_n(h_data, width * height, make_float4(0.0f));
-	h_data[width*(height / 2) + width / 4 + 1].x = 5;
-	h_data[width*(height / 2 + 1) + width / 4 + 1].x =5;
-	h_data[width*(height / 2 + 1) + width / 4].x = 5;
-	h_data[width*(height / 2) + width / 4].x = 5;
+	std::fill_n(h_pressureData, width * height, 0.0f);
+	h_pressureData[width*(height / 2) + width / 4 + 1] = 5;
+	h_pressureData[width*(height / 2 + 1) + width / 4 + 1] = 5;
+	h_pressureData[width*(height / 2 + 1) + width / 4] = 5;
+	h_pressureData[width*(height / 2) + width / 4] = 5;
+
+	std::fill_n(h_velocityData, width * height, make_float2(0.0f));
 	
 	// Allocate CUDA arrays in device memory
-	cudaChannelFormatDesc channelDesc = cudaCreateChannelDesc<float4>();
+	cudaChannelFormatDesc velocityChannelDesc = cudaCreateChannelDesc<float2>();
+	cudaChannelFormatDesc pressureChannelDesc = cudaCreateChannelDesc<float>();
 
-	cudaArray* cuInputArray;
-	CUDA(cudaMallocArray(&cuInputArray, &channelDesc, width, height, cudaArraySurfaceLoadStore));
+	cudaArray* cuPressureArray;
+	CUDA(cudaMallocArray(&cuPressureArray, &pressureChannelDesc, width, height, cudaArraySurfaceLoadStore));
 
-	cudaArray* cuOutputArray;
-	CUDA(cudaMallocArray(&cuOutputArray, &channelDesc, width, height, cudaArraySurfaceLoadStore));
+	cudaArray* cuVelocityArray;
+	CUDA(cudaMallocArray(&cuVelocityArray, &velocityChannelDesc, width, height, cudaArraySurfaceLoadStore));
 
 	// Copy to device memory some data located at address h_data
 	// in host memory 
-	CUDA(cudaMemcpyToArray(cuInputArray, 0, 0, h_data, size, cudaMemcpyHostToDevice));
+	CUDA(cudaMemcpyToArray(cuPressureArray, 0, 0, h_pressureData, pressureArraySize, cudaMemcpyHostToDevice));
+
+
+	CUDA(cudaMemcpyToArray(cuVelocityArray, 0, 0, h_velocityData, velocityArraySize, cudaMemcpyHostToDevice));
+
+	
 
 	// Bind the arrays to the surface references
-	CUDA(cudaBindSurfaceToArray(inputSurfRef, cuInputArray));
-	CUDA(cudaBindSurfaceToArray(outputSurfRef, cuOutputArray));
+	CUDA(cudaBindSurfaceToArray(velocitySurface, cuVelocityArray));
+	CUDA(cudaBindSurfaceToArray(pressureSurface, cuPressureArray));
 
-	texRef.normalized = true;
-	texRef.filterMode = cudaFilterModeLinear;
-	texRef.addressMode[0] = cudaAddressModeClamp; // extend at border
-	texRef.addressMode[1] = cudaAddressModeClamp;
-	texRef.addressMode[2] = cudaAddressModeClamp;
+	velocityTexRef.normalized = true;
+	velocityTexRef.filterMode = cudaFilterModeLinear;
+	velocityTexRef.addressMode[0] = cudaAddressModeClamp; // extend at border
+	velocityTexRef.addressMode[1] = cudaAddressModeClamp;
+	velocityTexRef.addressMode[2] = cudaAddressModeClamp;
 
-	CUDA(cudaBindTextureToArray(&texRef, cuInputArray, &channelDesc));
+	pressureTexRef.normalized = true;
+	pressureTexRef.filterMode = cudaFilterModeLinear;
+	pressureTexRef.addressMode[0] = cudaAddressModeClamp; // extend at border
+	pressureTexRef.addressMode[1] = cudaAddressModeClamp;
+	pressureTexRef.addressMode[2] = cudaAddressModeClamp;
+
+	CUDA(cudaBindTextureToArray(&pressureTexRef, cuPressureArray, &pressureChannelDesc));
+	CUDA(cudaBindTextureToArray(&velocityTexRef, cuVelocityArray, &velocityChannelDesc));
 
 	// Invoke kernel
 	dim3 dimBlock(32, 32);
@@ -255,48 +248,44 @@ int main()
 
 	firstVelocityIteration << <dimGrid, dimBlock >> > (width, height);
 	CUDA(cudaDeviceSynchronize());
-	writeBack << <dimGrid, dimBlock >> > (width, height);
-	CUDA(cudaDeviceSynchronize());
 
 	for (int i = 0; i < ITERATIONS; i++)
 	{
 		iteratePressure<< <dimGrid, dimBlock >> > (width, height);
 		CUDA(cudaDeviceSynchronize());
-		writeBack << <dimGrid, dimBlock >> > (width, height);
-		CUDA(cudaDeviceSynchronize());
 
 		iterateVelocity<< <dimGrid, dimBlock >> > (width, height);
-		CUDA(cudaDeviceSynchronize());
-		writeBack << <dimGrid, dimBlock >> > (width, height);
 		CUDA(cudaDeviceSynchronize());
 	}
 	
 
 	CUDA(cudaPeekAtLastError());
 
-	CUDA(cudaMemcpyFromArray(h_data, cuOutputArray, 0, 0, size, cudaMemcpyDeviceToHost));
+	CUDA(cudaMemcpyFromArray(h_pressureData, cuPressureArray, 0, 0, pressureArraySize, cudaMemcpyDeviceToHost));
+	CUDA(cudaMemcpyFromArray(h_velocityData, cuVelocityArray, 0, 0, velocityArraySize, cudaMemcpyDeviceToHost));
 
 	// Free device memory
-	CUDA(cudaFreeArray(cuInputArray));
-	CUDA(cudaFreeArray(cuOutputArray));
+	CUDA(cudaFreeArray(cuPressureArray));
+	CUDA(cudaFreeArray(cuVelocityArray));
 
-	writeOutput(h_data, width, height, "output.ppm");
+	writeOutput(h_pressureData, width, height, "output.ppm");
 	
-	delete[] h_data;
+	delete[] h_pressureData;
+	delete[] h_velocityData;
 
 	return 0;
 }
 
-void writeOutput(float4* data, int width, int height, const std::string& path)
+void writeOutput(float* data, int width, int height, const std::string& path)
 {
 	float* output = new float[width * height * 3];
 
 	for (int i = 0; i < width * height; i++)
 	{
-		float value = clamp(data[i].x * 0.5f + 0.5f, 0.0f, 1.0f);
+		float value = clamp(data[i] * 0.5f + 0.5f, 0.0f, 1.0f);
 		output[3 * i + 0] = value;
 		output[3 * i + 1] = value;
-		output[3 * i + 2] = value; //sqrt(1 - value1 * value1 - value2 * value2);
+		output[3 * i + 2] = value;
 	}
 
 	ppm(output, width, height, "output.ppm");
